164f573a1474d74ce41a53796fb154203e78098b
Muhammad Eagel Triutama <muhammadeagel@gmail.com>
2025-05-01 09:36:10 +0700

    menambahkan navbar

--- src/beranda/Beranda.jsx
+++ src/beranda/Beranda.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 export default function Beranda() {
   return (
     <div>
+        <Navbar />
         Ini halaman beranda
         <h1>Selamat datang di halaman beranda</h1>
     </div>
