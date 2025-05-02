bdfe6b7f4f66193406e4c7fd4d801a2e811e1637
Muhammad Eagel Triutama <muhammadeagel@gmail.com>
2025-05-02 09:38:54 +0700

    small fix layanan

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Beranda from "./beranda/Beranda";
 import Tentang from "./tentang/Tentang";
-import Layanan from "./layanan/layanan";
+import Layanan from "./layanan/Layanan";
 import Kontak from "./kontak/Kontak";
 import Layout from "./components/Layout/Layout";
 
