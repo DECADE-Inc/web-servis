54296836021d9a804ce754987728c70138706c88
Muhammad Eagel Triutama <muhammadeagel@gmail.com>
2025-05-02 09:33:28 +0700

    nambah wm

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { Outlet } from 'react-router'
-import Navbar from '../Navbar'
-import Footer from '../Footer'
+import React from "react";
+import { Outlet } from "react-router";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+import logoKomunitas from "../assets/logo_komunitas.jpg";
 
 export default function Layout() {
   return (
@@ -9,6 +10,10 @@ export default function Layout() {
       <Navbar />
       <Outlet />
       <Footer />
+      <div className="fixed bottom-10 left-10 flex items-center gap-4 rounded-lg bg-white p-4 text-4xl font-bold shadow-lg border-blue-500 border-4">
+        <img className="h-16 w-16 rounded-full" src={logoKomunitas} alt="" />
+        Preview
+      </div>
     </>
-  )
+  );
 }
